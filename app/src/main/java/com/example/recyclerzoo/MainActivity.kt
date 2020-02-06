ba3e0a9f8eb3d81a51c4da99c8e9caaec0642dae
ebrunso <eric.brunson@enhanceit.us>
2020-02-06 01:44:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recyclerzoo/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.recyclerzoo
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.core.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+//Create an app which is a directory for all the animals in a zoo.
+//1. Activity one: Make a homepage for zoo information
+//2. Activity two: ListView with list of all the categories.
+//3. Activity three: RecyclerView with a list of all the animals in that category
+//4. Activity four: Detail of that animal selected from the list.
+//     It should have the detail of that animal and play a sound of that animal.
+
+class MainActivity : AppCompatActivity() {
+
+    val drawable: Drawable? = ResourcesCompat.getDrawable(resources, R.drawable.zoostockimage, null)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun onClick(view: View){
+        when (view.id){
+            R.id.btnZooEntry -> {
+                intent = Intent(this, Species::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/recyclerzoo/Species.kt
@@ -0,0 +1,47 @@
+package com.example.recyclerzoo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_species.*
+
+//2. Activity two: ListView with list of all the categories.
+
+class Species : AppCompatActivity() {
+
+    val list by lazy {initList()}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_species)
+
+        initListView()
+    }
+
+    fun initListView(){
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list)
+        lvZooView.adapter = adapter
+        lvZooView.setOnItemClickListener{ _, _, i, _ ->
+            val selectedString = list[i]
+            val intent = Intent("MAKE>THE>INTENT") //Not a real intent
+            intent.putExtra("KEY", selectedString)
+            startActivity(intent)
+        }
+    }
+
+    fun initList() : ArrayList<String>{
+        val stringList = ArrayList<String>()
+
+        stringList.add("Amphibian")
+        stringList.add("Fish")
+        stringList.add("Reptile")
+        stringList.add("Mammal")
+        stringList.add("Bird")
+
+        return stringList
+    }
+
+
+}
